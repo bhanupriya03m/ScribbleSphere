6c488e8e6866d8d79dcaff5c746e10f651bebfee
bhanupriya03m <bhanupriyacit@gmail.com>
2024-05-06 23:19:51 +0530

    first commit

---
+++ api/db.js
@@ -0,0 +1,8 @@
+import mysql from "mysql2";
+
+export const db = mysql.createConnection({
+  host: "bfrlgk7kjsqfbmowswqf-mysql.services.clever-cloud.com",
+  user: "uvbi6atdwus28yic",
+  password: "QyohzrKbSsLdlUblF47m",
+  database: "bfrlgk7kjsqfbmowswqf",
+});
